8ad75d27a8f9fd7f9961ad68b4186513d7895ee0
headstock <coltnovak09@gmail.com>
2025-04-29 20:38:07 -0400

    Update lua/plugins/treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,92 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-          require("config.treesitter")
+  {
+    "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons"
+    },
+    config = function()
+      require("config.telescope").setup()
+
+      local builtin = require("telescope.builtin")
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find Files" })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live Grep" })
+    end,
+  },
+
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup({
+        size = 15,
+        open_mapping = [[<c-\>]],
+        hide_numbers = true,
+        shade_filetypes = {},
+        shade_terminals = true,
+        shading_factor = '1',
+        start_in_insert = true,
+        insert_mappings = true,
+        terminal_mappings = true,
+        persist_size = true,
+        direction = "horizontal",
+        close_on_exit = true,
+        shell = vim.o.shell,
+        auto_scroll = true,
+      })
+  
+      vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter", "TermEnter" }, {
+        pattern = "term://*",
+        callback = function()
+          if vim.bo.buftype == "terminal" then
+            vim.cmd("startinsert!")
+          end
         end,
-      },
-    
+      })
+    end,
+  },
+  {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons", 
+    },
+    opts = {
+      show_modified = true,
+      theme = 'auto',
+      attach_navic = true,
+    },
+  },
+  
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",  
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify"
+    },
+    config = function()
+      require("noice").setup({
+        lsp = {
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+        },
+        presets = {
+          bottom_search = false,  
+          command_palette = true,
+          long_message_to_split = true,  
+          inc_rename = false,  
+          lsp_doc_border = true, 
+        },
+      })
+
+      vim.notify = require("notify")
+    end
+  },
 }
