450354bc1dc975959943c70764bba8d99f5bb42a
Colt Novak <colton.novak@students.coderva.org>
2025-05-15 14:03:04 -0400

    Update ui.lua

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -32,6 +32,8 @@ return {
     config = function()
       require("toggleterm").setup({
         open_mapping = [[<c-\>]],
+                  direction = "horizontal",
+
         float_opts = {
           border = "curved",
           winblend = 3,
